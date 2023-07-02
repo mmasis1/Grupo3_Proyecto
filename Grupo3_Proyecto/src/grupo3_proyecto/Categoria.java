d9062300f75441585aafe58ab68a47db98b339c6
JuanMa113 <jmora40168@ufide.ac.cr>
2023-07-02 15:33:56 -0600

    clase de categoria y main categoria

---
+++ Grupo3_Proyecto/src/grupo3_proyecto/Categoria.java
@@ -0,0 +1,25 @@
+
+package grupo3_proyecto;
+public class Categoria {
+    private int IdCategoria;
+    public String NombreCategoria;
+    
+    
+    
+    public void setNombreCategoria(String NombreCategoria){
+    this.NombreCategoria = NombreCategoria;
+}
+   public String getNombreCategoria(){
+    return NombreCategoria;
+}
+   public void setIdCategoria(int IdCategoria) {
+        this.IdCategoria = IdCategoria;
+    
+    }
+public int getIdCategoria() {
+        IdCategoria = IdCategoria +1;
+        return IdCategoria;
+    
+    }
+
+}
